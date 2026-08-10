4a2bee93e15105324c0d4fd96442ee04e7baadb0
harshitmittal010 <harshitmittalkk77@gmail.com>
2026-08-10 20:53:49 +0530

    Day 01 programs

---
+++ Day-01/Q2.c
@@ -0,0 +1,21 @@
+// Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d\n", a + b);
+    printf("Difference = %d\n", a - b);
+    printf("Product = %d\n", a * b);
+
+     if (b != 0)
+        printf("Quotient = %d\n", a / b);
+    else
+        printf("Quotient cannot be calculated (division by zero).\n");
+
+    return 0;
+}
