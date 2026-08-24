96370fd499cf44ed9c12be7e7cfb7d246fec79b1
Harshit Mittal <harshitmittalkk77@gmail.com>
2026-08-24 19:58:02 +0530

    Day 15 programs

---
+++ Day-15/Q29.c
@@ -0,0 +1,23 @@
+// Write a program to calculate the factorial of a number.
+
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    long long factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.");
+    } else {
+        for (int i = 1; i <= n; i++)
+            factorial *= i;
+
+        printf("Factorial = %lld", factorial);
+    }
+
+    return 0;
+}
