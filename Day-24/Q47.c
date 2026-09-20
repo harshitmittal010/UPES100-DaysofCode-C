e07631390eceade30e0b225f260ef1980c1b0e92
Harshit Mittal <harshitmittalkk77@gmail.com>
2026-09-20 22:14:16 +0530

    Day 24 programs

---
+++ Day-24/Q47.c
@@ -0,0 +1,10 @@
+// Q47: Print an increasing star triangle.
+#include <stdio.h>
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++)
+             printf("*");
+        printf("\n");
+    }
+    return 0;
+}
