39de912b9bf8cead5ee2e8563c2bf558fd98862b
Harshit Mittal <harshitmittalkk77@gmail.com>
2026-09-20 22:26:50 +0530

    Day 26 programs

--- Day-26/Q52.c
+++ Day-26/Q52.c
@@ -1,19 +1,48 @@
-// Q52: Print the star pattern shown in the question.
-// Groups contain 1, 3, 5, 3, and 1 stars, with one star per line.
-#include <stdio.h>
-
-int main(void) {
-    for (int group = 1; group <= 5; group++) {
-        int stars = (group <= 3) ? 2 * group - 1 : 2 * (6 - group) - 1;
-
-        for (int star = 1; star <= stars; star++) {
-            printf("*\n");
-        }
-
-        if (group < 5) {
-            printf("\n");
-        }
-    }
-
-    return 0;
+/* 
+Q52 (Nested Loops without Arrays/Strings) 
+Write a program to print the following pattern: 
+ 
+* 
+ 
+* 
+* 
+* 
+ 
+* 
+* 
+* 
+* 
+* 
+ 
+* 
+* 
+* 
+ 
+* 
+*/ 
+ 
+#include <stdio.h> 
+ 
+int main() 
+{ 
+    int i, j; 
+ 
+    for (i = 1; i <= 5; i += 2) 
+    { 
+        for (j = 1; j <= i; j++) 
+            printf("*\n"); 
+ 
+        printf("\n"); 
+    } 
+ 
+    for (i = 3; i >= 1; i -= 2) 
+    { 
+        for (j = 1; j <= i; j++) 
+            printf("*\n"); 
+ 
+        if (i != 1) 
+            printf("\n"); 
+    } 
+ 
+    return 0; 
 }
